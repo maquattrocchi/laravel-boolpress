54ca245c61d752168df961f2816d30f4269b037d
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-06-24 14:55:35 +0200

    creazione model, migration e seeder tabella tags

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Tag;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['Vegetariano', 'Vegano', 'Senza Glutine', 'Carne', 'Pesce'];
+        foreach($tags as $tag){
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::of($newTag->name)->slug('-');
+            $newTag->save();
+        }
+    }
+}
